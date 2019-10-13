e920887b51490bbfd59b6057388ae54e7c302faa
JakeLarimore <43737137+JakeLarimore@users.noreply.github.com>
2019-10-13 09:50:58 -0500

    h and cpp file for temp function

---
+++ src/temp.cpp
@@ -0,0 +1,25 @@
+#include "temp.h"
+#include <Adafruit_NeoPixel.h>
+
+
+Temp::Temp(Adafruit_NeoPixel& strip) : strip{strip} {}
+
+void Temp::loop(apitemp) {
+  // put your main code here, to run repeatedly:
+  uint8_t celsius = apitemp - 273;
+  if (celsius >= 0) {
+    for (uint8_t i = 0; i < LED_COUNT; i++) {
+        if (3*temp[i] >= celsius) {
+            strip.setPixelColor(255,0,0, temp[i]);
+        }
+    }
+ } else {
+     for (uint8_t i = 16; i < LED_COUNT; i--) {
+        if (3*temp[i] >= celsius) {
+            strip.setPixelColor(126,126,255, temp[i]);
+        }
+    }
+ }
+  strip.show();
+  delay(10);
+}
---
+++ src/temp.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Adafruit_NeoPixel.h"
+
+#define LED_PIN 6
+#define LED_COUNT 16
+
+class Temp {
+    public:
+        Temp(Adafruit_NeoPixel& strip);
+        void loop(uint8_t apitemp);
+    private:
+        Adafruit_NeoPixel& strip;
+        uint8_t temp[LED_COUNT] = {0};
+};
